4d4d08745a940d8d9d6e648a55bf7b77b0a7cca0
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-19 19:21:44 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,8 +1,14 @@
 #include "StringCalculator.h"
+#include <string>
 
 int StringCalculator::add(const std::string& input) {
     if (input.empty()) {
         return 0;  // Return 0 for empty input
     }
+    
+    if (input == "0") {
+        return 0;  // Return 0 for "0" input
+    }
+    
     return 0;
 }
