22100d6ff6b1ad204b07cfe082d49c05a39b4e99
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-19 19:47:24 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,10 +2,15 @@
 #define STRINGCALCULATOR_H
 
 #include <string>
+#include <vector>
 
 class StringCalculator {
 public:
     int add(const std::string& input);
+
+private:
+    std::vector<int> splitAndConvert(const std::string& input);
+    void validateNegatives(const std::vector<int>& numbers);
 };
 
 #endif // STRINGCALCULATOR_H
